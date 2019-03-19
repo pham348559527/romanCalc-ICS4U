424cee53e26fa8bf2b795a368df8aafa4728de67
Tommy Pham (348559527) <348559527@gapps.yrdsb.ca>
2019-03-19 02:53:11 -0400

    testing

---
+++ AndroidStudioProjects/LaurierRoboticsDroneController/app/src/main/java/com/example/brons/laurier_robotics_drone_controller/ControllerLayout.java
@@ -0,0 +1,13 @@
+package com.example.brons.laurier_robotics_drone_controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ControllerLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controller_layout);
+    }
+}
